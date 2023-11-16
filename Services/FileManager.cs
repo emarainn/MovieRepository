ec33d616f2562ee5c2365cec111a241b4efcfcf9
emarainn <enimphius1@my.wctc.edu>
2023-11-16 17:50:52 -0600

    Final

--- Services/FileManager.cs
+++ Services/FileManager.cs
@@ -12,37 +12,10 @@ public partial class MainService
             int movieID = findMaxId(filename);
 
             Console.WriteLine($"Movie ID: {movieID}");
-            string movieTitle;
-            var ans = 0;
 
-            do {
-                Console.WriteLine("Enter Movie Title:");
-                movieTitle = Console.ReadLine();
+            string movieTitle = FindTitle(filename);
 
-                var lines = File.ReadLines(filename).Skip(1);
-
-                foreach (string line in lines)
-                {
-                    ans = line.IndexOf(movieTitle);
-
-                    if (ans >= 0)
-                    {
-                        Console.WriteLine("Title Already Entered");
-                        break;
-                    }
-                }
-            } while (ans >= 0);
-
-            Console.WriteLine("Enter Number of Genres: ");
-            var num = Convert.ToInt32(Console.ReadLine());
-
-            string[] movieGenre = new string[num];
-
-            for (int i = 0; i < num; i++)
-            {
-                Console.WriteLine("Type in Genre: ");
-                movieGenre[i] = Console.ReadLine();
-            }
+            string[] movieGenre = FindGenres(filename);
 
             IMovie movie = new Movies(movieID, movieTitle, movieGenre);
             StreamWriter sw = new StreamWriter(filename, true);
@@ -53,8 +26,14 @@ public partial class MainService
         //Display SPECIFIC number of movies starting from 1st
         public void Read(string filename)
         {
+            int numItem;
             Console.Write("Enter amount of movies you want to view: ");
-            int numItem = Convert.ToInt32(Console.ReadLine());
+            
+            while (!int.TryParse(Console.ReadLine(), out numItem) || numItem <= 0)
+            {
+                Console.WriteLine("**Must Enter Number Greater Than 0**");
+                Console.WriteLine("Enter amount of movies you want to view: ");
+            }
 
             //Shows the "titles" of the rows
             string items = File.ReadLines(filename).Skip(0).Take(1).First();
@@ -71,6 +50,7 @@ public partial class MainService
                 Console.WriteLine(line);
             }
         }
+
         public int findMaxId(string filename)
         {
             var maxId = 0;
@@ -87,71 +67,69 @@ public partial class MainService
                 {
                     maxId = newid;
                 }
+                maxId++;
             }
             return maxId;
         }
 
+        public string FindTitle(string filename)
+        {
+            string movieTitle;
+            var ans = 0;
+
+            do
+            {
+                Console.WriteLine("Enter Movie Title:");
+                movieTitle = Console.ReadLine();
+
+                if (movieTitle == "")
+                {
+                    Console.WriteLine("**Must Enter Movie Title**");
+                }
+
+                var lines = File.ReadLines(filename).Skip(1);
+
+                foreach (string line in lines)
+                {
+                    ans = line.IndexOf(movieTitle);
+
+                    if (ans >= 0 && movieTitle != "")
+                    {
+                        Console.WriteLine("Title Already Exists");
+                        break;
+                    }
+                    
+                }
+            } while (ans >= 0 || movieTitle == "");
+
+            return movieTitle;
+        }
+
+        public string[] FindGenres(string filename)
+        {
+            int num;
+            Console.WriteLine("Enter Number of Genres: ");
+            
+            while (!int.TryParse(Console.ReadLine(), out num) || num <= 0) 
+            {  
+                Console.WriteLine("**Must Enter Number Greater Than 0**");
+                Console.WriteLine("Enter Number of Genres: ");
+            }
+
+
+            //I don't know why I cant do input validation :(
+            string[] movieGenre = new string[num];
+            int i = 0;
+
+                for (i = 0; i < num; i++)
+                {
+                    Console.WriteLine("Type in Genre: ");
+                    movieGenre[i] = Console.ReadLine();
+
+                }
+           
+            return movieGenre;
+
+        }
     }
 }
-
-/*
-
-                //find title
-                string title = line.Split(",")[1];
-                string line1 = "1, Toy Story (2000), Action|Comedy";
-                var user = "Toy Story (2000)";
-                var movieExists = line.IndexOf(user);
-
-                String.Compare(line1, user, StringComparison.InvariantCultureIgnoreCase);
-                line1.StartsWith(user);
-
- */
-
-
-
-
-//OG Code from Invoke
-/*        
-        var menu = new Menu();
-
-        Menu.MenuOptions menuChoice;
-        do
-        {
-            menuChoice = menu.ChooseAction();
-
-            switch (menuChoice)
-            {
-                case Menu.MenuOptions.ListFromStudentRepository:
-                    _repository.Read();
-                    _repository.Display();
-                    break;
-                case Menu.MenuOptions.ListFromFileRepository:
-                    var factory1 = LoggerFactory.Create(b => b.AddConsole());
-                    var logger1 = factory1.CreateLogger<FileRepository>();
-                    var fileRepository1 = new FileRepository(logger1);
-                    fileRepository1.Read();
-                    fileRepository1.Display();
-                    break;
-                case Menu.MenuOptions.Add:
-                    _logger.LogInformation("Adding a new movie");
-                    var factory2 = LoggerFactory.Create(b => b.AddConsole());
-                    var logger2 = factory2.CreateLogger<FileRepository>();
-                    var fileRepository2 = new FileRepository(logger2);
-                    fileRepository2.Read();
-                    fileRepository2.Write(999999, "My Title", "Horror|Action");
-                    //menu.GetUserInput();
-                    break;
-                case Menu.MenuOptions.Update:
-                    _logger.LogInformation("Updating an existing movie");
-                    break;
-                case Menu.MenuOptions.Delete:
-                    _logger.LogInformation("Deleting a movie");
-                    break;
-            }
-        }
-        while (menuChoice != Menu.MenuOptions.Exit);
-
-        menu.Exit();
-
-        Console.WriteLine("\nThanks for using the Movie Library!");
-*/
--- Services/IMovie.cs
+++ Services/IMovie.cs
@@ -4,58 +4,8 @@ public partial class MainService
 {
     public interface IMovie
     {
-        //public int MovieID { get; set; }
         public int MovieID { get; set; }
         public string MovieTitle { get; set; }
         public string MovieGenre { get; set; }
     }
-
-
-
-
 }
-
-/*        
-        var menu = new Menu();
-
-        Menu.MenuOptions menuChoice;
-        do
-        {
-            menuChoice = menu.ChooseAction();
-
-            switch (menuChoice)
-            {
-                case Menu.MenuOptions.ListFromStudentRepository:
-                    _repository.Read();
-                    _repository.Display();
-                    break;
-                case Menu.MenuOptions.ListFromFileRepository:
-                    var factory1 = LoggerFactory.Create(b => b.AddConsole());
-                    var logger1 = factory1.CreateLogger<FileRepository>();
-                    var fileRepository1 = new FileRepository(logger1);
-                    fileRepository1.Read();
-                    fileRepository1.Display();
-                    break;
-                case Menu.MenuOptions.Add:
-                    _logger.LogInformation("Adding a new movie");
-                    var factory2 = LoggerFactory.Create(b => b.AddConsole());
-                    var logger2 = factory2.CreateLogger<FileRepository>();
-                    var fileRepository2 = new FileRepository(logger2);
-                    fileRepository2.Read();
-                    fileRepository2.Write(999999, "My Title", "Horror|Action");
-                    //menu.GetUserInput();
-                    break;
-                case Menu.MenuOptions.Update:
-                    _logger.LogInformation("Updating an existing movie");
-                    break;
-                case Menu.MenuOptions.Delete:
-                    _logger.LogInformation("Deleting a movie");
-                    break;
-            }
-        }
-        while (menuChoice != Menu.MenuOptions.Exit);
-
-        menu.Exit();
-
-        Console.WriteLine("\nThanks for using the Movie Library!");
-*/
